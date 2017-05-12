9c11797731198082cab721bd60a618a5295ee8ab
kcolin14 <kevin.colin@mapquest.com>
2017-05-11 23:40:00 -0600

    Added mapquest mapping sdk & setup mapview in MainActivity

--- app/src/main/java/com/keco1249/mapquestmappingsdktestapp/MainActivity.java
+++ app/src/main/java/com/keco1249/mapquestmappingsdktestapp/MainActivity.java
@@ -3,11 +3,60 @@ package com.keco1249.mapquestmappingsdktestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapquest.mapping.MapQuest;
+import com.mapquest.mapping.maps.MapView;
+import com.mapquest.mapping.maps.MapboxMap;
+import com.mapquest.mapping.maps.OnMapReadyCallback;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
+    private MapboxMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Call this right after calling the parent class's onCreate method
+        MapQuest.start(this);
+
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
